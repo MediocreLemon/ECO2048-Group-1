b16fbd174f434da8f2facb9f27a14292abebac3a
MediocreLemon <99483564+MediocreLemon@users.noreply.github.com>
2024-11-19 16:01:45 +0000

    Add files via upload

--- ECO2048_Group_1.m
+++ ECO2048_Group_1.m
@@ -1,5 +1,5 @@
 %TRACE Project
-
+clear all
 %1 - Randomly select 10 countries from the data provided
 rng(1,'twister') % sets the random seed to the group number
 load 'available_countries.mat' % loads the list of available countries to choose from
@@ -15,10 +15,35 @@ while hasdata(ds) % while there is unread data in the datastore
     segment = read(ds); % reads the datastore
     isSelected = ismember(segment.i,selectedCountries) & ismember(segment.j,selectedCountries); % holds data where the I and J match something from the list of seleted countries
     adjustedSegment = segment(isSelected,:);
-    adjustedData = [adjustedData; adjustedSegment]; % fills the adjustedData matrix with data stored under i,j,k, and v for the selected countries
+    adjustedData = [adjustedData; adjustedSegment]; %#ok<AGROW> % fills the adjustedData matrix with data stored under i,j,k, and v for the selected countries
 end
 
 %4 obtain the total value of V for each pair of nations
 
+% Create a table to store the data
+tradeData = table(adjustedData.i, adjustedData.j, adjustedData.k, adjustedData.v, 'VariableNames', {'Origin', 'Destination', 'Product Code', 'Value'}); 
+%technically redudant, but keeps the variable seperate and introduces
+%cleaner variable names
+ 
+% create a matrix of all unique pairs of exporters and importers
+pairs = unique([adjustedData.i, adjustedData.j], 'rows');
 
+% pre-initialize a result matrix
+tradeValueResult = zeros(size(pairs, 1), 3);
 
+for k = 1:size(pairs, 1)
+    origin = pairs(k, 1); %origin nation is the first column
+    destination = pairs(k, 2); %destination nation is the second column
+    
+    % Find all rows corresponding to this pair
+    pairRow = tradeData.Origin == origin & tradeData.Destination == destination;
+    
+    % Sum the trade values for this pair
+    totalValue = sum(tradeData.Value(pairRow));
+    
+    % Store the result
+    tradeValueResult(k, :) = [origin, destination, totalValue];
+end
+
+%5 build a matrix to determine which countries trade with the others
+% ignore countries trading with themselves
